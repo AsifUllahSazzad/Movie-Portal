f9d9f4471916bb6de0014c7294b83b7f14140388
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-08-15 14:10:01 +0600

    Update Movies components

--- src/private/AddMovies.jsx
+++ src/private/AddMovies.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React from "react";
 
 const AddMovies = () => {
-    return (
-        <div>
-            
+  const genres = [
+    "Comedy",
+    "Drama",
+    "Horror",
+    "Action",
+    "Thriller",
+    "Romance",
+    "Sci-Fi",
+    "Animation",
+  ];
+
+  
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Add Movie</h1>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Movie Poster (URL)</label>
+              <input type="url" className="input" placeholder="https://" />
+
+              <label className="label">Movie Title</label>
+              <input
+                type="text"
+                className="input"
+                placeholder="e.g. Inception"
+              />
+
+              <label className="label">Genre</label>
+              <select defaultValue="Pick a color" className="select">
+                <option value="">Select genre</option>
+                {
+                    genres.map((g) => <option key={g} value={g}>{g}</option>)
+                }
+              </select>
+
+              <label className="label">Duration</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <label className="label">Release Year</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <label className="label">Rating</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <label className="label">Summary</label>
+              <input type="password" className="input" placeholder="Password" />
+
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddMovies;
