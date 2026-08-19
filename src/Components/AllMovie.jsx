b50227921003bb8a57728b8deaea58ecaa29f3fd
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-08-20 02:41:50 +0600

    fix: update movie component

--- src/Components/AllMovie.jsx
+++ src/Components/AllMovie.jsx
@@ -14,7 +14,16 @@ const AllMovie = ({ movie }) => {
     Email,
   } = movie;
 
-  console.log(Duration);
+  // convert hours
+  const formatDuration = (minutes) => {
+    const mins = Number(minutes);
+    if (!mins || isNaN(mins)) return "";
+
+    const hours = Math.floor(mins / 60);
+    const remainingMins = mins % 60;
+
+    return `${hours}h ${remainingMins}min`;
+  };
 
   return (
     <div className="card bg-base-100 w-96 shadow-2xl">
@@ -24,13 +33,22 @@ const AllMovie = ({ movie }) => {
       >
         <img src={moviePoster} alt="Shoes" />
       </figure>
-      <div className="card-body">
+      <div className="card-body -mt-3">
         <h2 className="card-title text-3xl font-normal">{movieTitle}</h2>
-       <div>
-         <p>
-          {/* {Summary} */}
-        </p>
-       </div>
+        <div className="space-x-2 -mt-2 text-base text-[#818181] font-semibold">
+          <span>{Genre} /</span>
+
+          <span>{formatDuration(Duration)} /</span>
+
+          <span>{releaseYear}</span>
+        </div>
+
+        <div className="text-base flex justify-between items-center">
+          <span className="uppercase text-[#e7e7e7]">Summary</span>
+          <span>{Rating}</span>
+        </div>
+        {/* <p>{Summary}</p> */}
+
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Buy Now</button>
         </div>
